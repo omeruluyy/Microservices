e133ecb386a8339aa4d57f2048dbfc2651691168
Omer <omeruluy@hotmail.com>
2023-06-26 12:06:09 +0300

    IdentityServer has been implemented

---
+++ IdentityServer/FreeCourse.IdentityServer/Controllers/NoContent.cs
@@ -0,0 +1,6 @@
+﻿namespace FreeCourse.IdentityServer.Controllers
+{
+    internal class NoContent
+    {
+    }
+}
---
+++ IdentityServer/FreeCourse.IdentityServer/Controllers/UserController.cs
@@ -0,0 +1,42 @@
+﻿using FreeCourse.IdentityServer.Dtos;
+using FreeCourse.IdentityServer.Models;
+using FreeCourse.Shared.Dtos;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+
+namespace FreeCourse.IdentityServer.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UserController : ControllerBase
+    {
+        private readonly UserManager<ApplicationUser> _userManager;
+
+
+        public UserController(UserManager<ApplicationUser> userManager)
+        {
+            _userManager = userManager;
+        }
+        [HttpPost]
+        public async Task<IActionResult> Signup (SignupDto signupDto)
+        {
+            var user = new ApplicationUser
+            {
+                UserName = signupDto.UserName,
+                Email = signupDto.Email,
+                City = signupDto.City,
+            };
+
+            var result =await _userManager.CreateAsync(user,signupDto.Password);
+
+            if (!result.Succeeded)
+            {
+                return BadRequest(Response<NoContent>.Fail(result.Errors))
+            }
+
+            return NoContent();
+        }
+    }
+}
